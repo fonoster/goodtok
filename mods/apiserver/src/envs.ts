622ab928f48fe1fc62a407374cba70bd17414bf4
Pedro Sanders <psanders@fonoster.com>
2023-10-18 23:04:32 -0400

    refactor: group and rename environment variables for clarity

--- mods/apiserver/src/envs.ts
+++ mods/apiserver/src/envs.ts
@@ -1,4 +1,3 @@
-/* eslint-disable prettier/prettier */
 /*
  * Copyright (C) 2023 by Fonoster Inc (https://fonoster.com)
  * http://github.com/fonoster/goodtok
@@ -36,15 +35,21 @@ export const NATS_URL = e.NATS_URL;
 export const APISERVER_BIND_PORT = e.APISERVER_BIND_PORT ?? "6789";
 
 // Security and Encryption
-export const JWT_SECURITY_SALT = e.JWT_SECURITY_SALT ?? crypto.randomBytes(4).toString("hex");
-export const JWT_SIGN_OPTIONS = e.JWT_SIGN_OPTIONS ? JSON.parse(e.JWT_SIGN_OPTIONS) : defaultSignOptions
+export const JWT_SECURITY_SALT =
+  e.JWT_SECURITY_SALT ?? crypto.randomBytes(4).toString("hex");
+export const JWT_SIGN_OPTIONS = e.JWT_SIGN_OPTIONS
+  ? JSON.parse(e.JWT_SIGN_OPTIONS)
+  : defaultSignOptions;
 export const SECURITY_PATH_TO_KEYS = e.SECURITY_PATH_TO_KEYS ?? "/keys";
-export const SECURITY_PRIVATE_KEY = fs.readFileSync(join(SECURITY_PATH_TO_KEYS, "private.key"), "utf8");
+export const SECURITY_PRIVATE_KEY = fs.readFileSync(
+  join(SECURITY_PATH_TO_KEYS, "private.key"),
+  "utf8"
+);
 export const CLOAK_ENCRYPTION_KEY = e.CLOAK_ENCRYPTION_KEY;
 
 // SIP configurations
 export const SIP_DOMAIN = e.SIP_DOMAIN ?? "sip.goodtok.io";
 export const SIP_DOMAIN_REF = e.SIP_DOMAIN_REF ?? "default";
-export const SIP_SIGNALING_SERVER = e.SIP_SIGNALING_SERVER ?? "wss://sip.goodtok.io:5063";
+export const SIP_SIGNALING_SERVER =
+  e.SIP_SIGNALING_SERVER ?? "wss://sip.goodtok.io:5063";
 export const SIP_USER_AGENT_PRIVACY = e.SIP_USER_AGENT_PRIVACY ?? "PRIVATE";
-
