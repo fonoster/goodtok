225aa8e584003ac8f7053a1ce893912e2812e89a
Pedro Sanders <psanders@fonoster.com>
2023-10-22 18:56:04 -0400

    chore: reorganized code for clarity

--- mods/widget/src/GoodtokComponent.tsx
+++ mods/widget/src/GoodtokComponent.tsx
@@ -25,13 +25,12 @@ import { Web } from "sip.js";
 import React from "react";
 
 class GoodtokComponent extends HTMLElement {
+  private simpleUser: Web.SimpleUser;
   constructor() {
     super();
   }
 
   connectedCallback() {
-    let simpleUser: Web.SimpleUser;
-
     const audioElement = document.createElement("audio");
     const styleSlot = document.createElement("section");
     const goodtokContainer = document.createElement("div");
@@ -44,11 +43,11 @@ class GoodtokComponent extends HTMLElement {
 
     const eventHandler = (event: GoodtokWidgetEvents) => {
       if (event === GoodtokWidgetEvents.VIDEO_SESSION_REQUEST) {
-        if (simpleUser) {
-          simpleUser
+        if (this.simpleUser) {
+          this.simpleUser
             .connect()
             .then(() => {
-              simpleUser.register({
+              this.simpleUser.register({
                 requestOptions: {
                   extraHeaders: [
                     "X-Connect-Token: eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJyZWYiOiJmcm9udC1vZmZpY2UtYWdlbnQiLCJkb21haW5SZWYiOiJkZWZhdWx0IiwiYW9yIjoic2lwOmFub255bW91c0BzaXAuZ29vZHRvay5pbyIsImFvckxpbmsiOiJzaXA6YW5vbnltb3VzQHNpcC5nb29kdG9rLmlvIiwiZG9tYWluIjoic2lwLmdvb2R0b2suaW8iLCJwcml2YWN5IjoiTk9ORSIsImFsbG93ZWRNZXRob2RzIjpbIlJFR0lTVEVSIl0sInNpZ25hbGluZ1NlcnZlciI6IndzOi8vc2lwLmdvb2R0b2suaW86NTA2MiIsImlhdCI6MTY5Nzk5ODI0MywiZXhwIjoxNjk4MDg0NjQzfQ.bRZL_pRRnxZpu-Wvq8Pvd_WG882Jg4cL5D5D30DC0-_I0V2s4kbhlE67DGOEWnj36OZeQ56Ui4r_TNu7kVgvUc_Q2yDn-cXGg9Kiwn1GSEny88BYmCTVL8L7y8hEfafObw1NYMMxOXTI-dYvLLbicvcaUxQAfndiUD5AvB1K1eqt1GA1RsP7sa4KQ3RMVzVm2_8OOshtmg5q2zWYa36G-CFXofS5hjzKuvLmVIeiUtlaA0ZR0a4PXAcFmimFscHerVEr3NKDpbawA0gOvjv9-iuYdJMlqZDOilENNBIhzmDhob97j_jnXMuMS2QW7P-EAl1GBf_sC6MjTOsY8r4iMx1Auoq7PmCs4puQH-DyJhGPtkR9gNm1uKC6hsWot4acAkRUptenktGr66Gotw4T_vss0a_VbkNC1xV_HqvzCGRpsNmARPWzZmaZNBAfIaOoBfQAcM3wGDQC9QdYQE7160ImQPRCLybm1eWb9ZBEjmNUY-uj3x1OPI1bjfA_EY_YvD3TApvhk0rL1SdXcsBRYSvGXVDsw7c26FRVa5Fzwm88oTY3GMuMWdepLGIeh0uspXcfLWxrea6tB7iqKoR7eB3OdLEaNG6QzVyT2HI0DGLPZxJo50xMqVtGHt_nABJTmVSk6sLHol2m70lLqHL90QHBIesIkeUTaop3VDjHAZk",
@@ -87,15 +86,15 @@ class GoodtokComponent extends HTMLElement {
       };
 
       // Fixme: Should get from Goodtok API
-      simpleUser = new Web.SimpleUser("ws://192.168.1.7:5062", options);
+      this.simpleUser = new Web.SimpleUser("ws://192.168.1.7:5062", options);
 
       const delegate = {
         onCallReceived: () => {
-          simpleUser.answer();
+          this.simpleUser.answer();
         }
       };
 
-      simpleUser.delegate = delegate;
+      this.simpleUser.delegate = delegate;
     };
 
     const root = createRoot(goodtokContainer);
