42d33c1e7e4b6f3bca8398f0c6e95cef7739ecb1
Pedro Sanders <psanders@fonoster.com>
2023-11-08 23:19:47 -0500

    chore: wip front office

--- mods/frontoffice/src/components/onboarding/OnboardingPage.tsx
+++ mods/frontoffice/src/components/onboarding/OnboardingPage.tsx
@@ -23,7 +23,30 @@ import { TextField } from "../textfield/TextField";
 import { Button } from "../button/Button";
 import { timezones } from "../../utils/timezones";
 import { Select } from "../../components/select/Select";
+import { Formik, Form, Field, useFormikContext } from "formik";
+import { z } from "zod";
+import { toFormikValidationSchema } from "zod-formik-adapter";
 import React, { useState } from "react";
+
+// Define the Zod validation schema for each step
+const stepValidationSchemas = [
+  z.object({
+    name: z.string().min(1, "Workspace name is required.")
+  }),
+  z.object({
+    shopifyUrl: z
+      .string()
+      .min(1, "Shopify URL is required.")
+      .url("Invalid URL"),
+    shopifyApiKey: z.string().min(1, "Shopify API key is required.")
+  }),
+  z.object({
+    calendarUrl: z
+      .string()
+      .min(1, "Calendar URL is required.")
+      .url("Invalid URL")
+  })
+];
 
 type OnboardingPageProps = {
   onAbandon: () => void;
@@ -43,190 +66,217 @@ export const OnboardingPage: React.FC<OnboardingPageProps> = ({
   onCreateWorkspace,
   ...props
 }) => {
-  const [activeStep, setActiveStep] = useState(0);
-  const [workspaceName, setWorkspaceName] = useState("");
-  const [shopifyUrl, setShopifyUrl] = useState("");
-  const [shopifyApiKey, setShopifyApiKey] = useState("");
   const [timezone, setTimezone] = useState("America/New_York");
-  const [calendarUrl, setCalendarUrl] = useState("");
+  const [activeStep, setActiveStep] = useState(0);
+
+  // Define initial form values
+  const initialValues = {
+    name: "",
+    shopifyUrl: "",
+    shopifyApiKey: "",
+    calendarUrl: ""
+  };
+
+  const formik = useFormikContext();
+
+  const currentValidationSchema = toFormikValidationSchema(
+    stepValidationSchemas[activeStep]
+  );
 
   return (
-    <Box {...props} display="flex" flexDirection="column" minHeight="100vh">
-      <AppBar
-        workspaceId=""
-        userName=""
-        avatar=""
-        // This will hide the avatar and its menu
-        isAuthenticated={false}
-        onSignOut={() => {}}
-      />
-      <Box
-        sx={{ pt: 5, display: "flex", justifyContent: "center" }}
-        minWidth="100%"
-      >
-        <Stepper nonLinear activeStep={activeStep} sx={{ width: 764 }}>
-          {steps.map((label, index) => (
-            <Step key={label} completed={false}>
-              <StyledStepButton onClick={() => setActiveStep(index)}>
-                <StepLabel>{label}</StepLabel>
-              </StyledStepButton>
-            </Step>
-          ))}
-        </Stepper>
-      </Box>
+    <Formik
+      initialValues={initialValues}
+      validationSchema={currentValidationSchema}
+      onSubmit={(values) => {
+        if (activeStep < steps.length - 1) {
+          setActiveStep(activeStep + 1); // Proceed to next step
+        } else {
+          onCreateWorkspace({
+            name: values.name,
+            timezone: timezone,
+            calendarUrl: values.calendarUrl,
+            shopifyUrl: values.shopifyUrl,
+            shopifyApiKey: values.shopifyApiKey
+          });
+        }
+      }}
+    >
+      {({ errors, touched }) => (
+        <Form noValidate>
+          <Box
+            {...props}
+            display="flex"
+            flexDirection="column"
+            minHeight="100vh"
+          >
+            <AppBar
+              workspaceId=""
+              userName=""
+              avatar=""
+              // This will hide the avatar and its menu
+              isAuthenticated={false}
+              onSignOut={() => {}}
+            />
+            <Box
+              sx={{ pt: 5, display: "flex", justifyContent: "center" }}
+              minWidth="100%"
+            >
+              <Stepper nonLinear activeStep={activeStep} sx={{ width: 764 }}>
+                {steps.map((label, index) => (
+                  <Step key={label} completed={false}>
+                    <StyledStepButton onClick={() => setActiveStep(index)}>
+                      <StepLabel>{label}</StepLabel>
+                    </StyledStepButton>
+                  </Step>
+                ))}
+              </Stepper>
+            </Box>
 
-      <Box sx={{ display: "flex", justifyContent: "center" }} minWidth="100%">
-        <Box mt={6}>
-          {activeStep === 0 && (
-            <Box sx={{ width: "440px" }}>
-              {" "}
-              {/* Align the Box on the parent Box */}
-              <OnboardingTitle sx={{ textAlign: "left" }}>
-                Create a Workspace to get started managing your queues and more.
-              </OnboardingTitle>
-              <OnboardingBody sx={{ mt: 6, textAlign: "left" }}>
-                Worspace is how you manage a property in Goodtok. With a
-                workspace you can invite members, access the customer queue,
-                enable integrations and more.
-              </OnboardingBody>
-              <TextField
-                sx={{ mt: 8, mb: 4 }}
-                label="Name"
-                placeholder="Workspace Name"
-                helperText="Please enter a name for your workspace"
-                value={workspaceName}
-                onChange={(e) => setWorkspaceName(e.target.value)}
-              />
-              <Select
-                label="Timezone"
-                helperText="Please select a timezone"
-                value={timezone}
-                data={timezones}
-                onChange={(e) => setTimezone(e.target.value)}
-              />
-              <Box
-                sx={{
-                  mt: 4,
-                  display: "flex",
-                  justifyContent: "space-between",
-                  width: 212,
-                  gap: 2
-                }}
-              >
-                <Button onClick={onAbandon} variant="outlined">
-                  Skip for now
-                </Button>
+            <Box
+              sx={{ display: "flex", justifyContent: "center" }}
+              minWidth="100%"
+            >
+              <Box mt={6}>
+                {activeStep === 0 && (
+                  <Box sx={{ width: "440px" }}>
+                    {" "}
+                    {/* Align the Box on the parent Box */}
+                    <OnboardingTitle sx={{ textAlign: "left" }}>
+                      Create a Workspace to get started managing your queues and
+                      more.
+                    </OnboardingTitle>
+                    <OnboardingBody sx={{ mt: 6, textAlign: "left" }}>
+                      Worspace is how you manage a property in Goodtok. With a
+                      workspace you can invite members, access the customer
+                      queue, enable integrations and more.
+                    </OnboardingBody>
+                    <Field
+                      as={TextField}
+                      label="Name"
+                      name="name"
+                      placeholder="Workspace Name"
+                      error={touched.name && !!errors.name}
+                      helperText={
+                        (touched.name && errors.name) ||
+                        "Please enter a name for your workspace"
+                      }
+                      sx={{ mt: 8, mb: 4 }}
+                    />
+                    <Select
+                      label="Timezone"
+                      data={timezones}
+                      helperText={"Please select a timezone"}
+                      onChange={(e) => setTimezone(e.target.value)}
+                      sx={{ mb: 4 }}
+                      value={timezone}
+                    />
+                    <Box
+                      sx={{
+                        mt: 4,
+                        display: "flex",
+                        justifyContent: "space-between",
+                        width: 212,
+                        gap: 2
+                      }}
+                    >
+                      <Button onClick={onAbandon} variant="outlined">
+                        Skip for now
+                      </Button>
 
-                <Button
-                  onClick={() => {
-                    setActiveStep(1);
-                  }}
-                  disabled={workspaceName.length === 0}
-                >
-                  Continue
-                </Button>
+                      <Button type="submit">Continue</Button>
+                    </Box>
+                  </Box>
+                )}
+                {activeStep === 1 && (
+                  <Box sx={{ width: "440px" }}>
+                    <OnboardingTitle sx={{ textAlign: "left" }}>
+                      Set your Shopify credentials.
+                    </OnboardingTitle>
+                    <OnboardingBody sx={{ mt: 2, textAlign: "left" }}>
+                      For information on how to obtain an API key for your
+                      integration integration please head over to our
+                      documentation and search for "Shopify API KEY."
+                    </OnboardingBody>
+
+                    {/* TODO: Validate that it is a valid shopify url */}
+                    <Field
+                      as={TextField}
+                      name="shopifyUrl"
+                      label="Store URL"
+                      placeholder="Shopify Store URL"
+                      error={touched.shopifyUrl && !!errors.shopifyUrl}
+                      helperText={
+                        (touched.shopifyUrl && errors.shopifyUrl) ||
+                        "Please enter your store URL"
+                      }
+                      sx={{ mt: 8 }}
+                    />
+
+                    <Field
+                      as={TextField}
+                      label="API Key"
+                      name="shopifyApiKey"
+                      placeholder="shpss_1234567890abcdef1234567890abcdef"
+                      type="password"
+                      error={touched.shopifyApiKey && !!errors.shopifyApiKey}
+                      helperText={
+                        (touched.shopifyApiKey && errors.shopifyApiKey) ||
+                        "Please enter your API Key"
+                      }
+                      sx={{ mt: 4 }}
+                    />
+
+                    <Box
+                      sx={{
+                        mt: 4,
+                        display: "flex",
+                        justifyContent: "space-between",
+                        width: 212,
+                        gap: 2
+                      }}
+                    >
+                      <Button onClick={onAbandon} variant="outlined">
+                        Skip for now
+                      </Button>
+
+                      <Button type="submit">Continue</Button>
+                    </Box>
+                  </Box>
+                )}
+                {activeStep === 2 && (
+                  <Box sx={{ width: "440px" }}>
+                    <OnboardingTitle sx={{ textAlign: "left" }}>
+                      Set your calendar URL.
+                    </OnboardingTitle>
+                    <OnboardingBody sx={{ mt: 2, textAlign: "left" }}>
+                      By clicking on the button below you agree to our privacy
+                      policy.
+                    </OnboardingBody>
+
+                    <Field
+                      as={TextField}
+                      label="Calendar URL"
+                      name="calendarUrl"
+                      placeholder="https://cal.com/some-calendar"
+                      error={touched.calendarUrl && !!errors.calendarUrl}
+                      helperText={
+                        touched.calendarUrl && errors.calendarUrl
+                          ? errors.calendarUrl
+                          : "Please enter your calendar URL"
+                      }
+                      sx={{ mt: 6 }}
+                    />
+
+                    <Button sx={{ mt: 4 }} type="submit">
+                      Finish
+                    </Button>
+                  </Box>
+                )}
               </Box>
             </Box>
-          )}
-          {activeStep === 1 && (
-            <Box sx={{ width: "440px" }}>
-              <OnboardingTitle sx={{ textAlign: "left" }}>
-                Set your Shopify credentials.
-              </OnboardingTitle>
-              <OnboardingBody sx={{ mt: 2, textAlign: "left" }}>
-                For information on how to obtain an API key for your integration
-                please head over to our documentation and search for "Shopify
-                API KEY."
-              </OnboardingBody>
-
-              {/* TODO: Validate that it is a valid shopify url */}
-              <TextField
-                sx={{ mt: 8 }}
-                label="Store URL"
-                placeholder="mystore-43c62e3b.myshopify.com"
-                helperText="Please enter your store URL"
-                value={shopifyUrl}
-                onChange={(e) => setShopifyUrl(e.target.value)}
-              />
-
-              <TextField
-                sx={{ mt: 4 }}
-                label="API Key"
-                placeholder="shpss_1234567890abcdef1234567890abcdef"
-                helperText="Please enter your API Key"
-                type="password"
-                value={shopifyApiKey}
-                onChange={(e) => setShopifyApiKey(e.target.value)}
-              />
-
-              <Box
-                sx={{
-                  mt: 4,
-                  display: "flex",
-                  justifyContent: "space-between",
-                  width: 212,
-                  gap: 2
-                }}
-              >
-                <Button onClick={onAbandon} variant="outlined">
-                  Skip for now
-                </Button>
-
-                <Button
-                  onClick={() => {
-                    setActiveStep(2);
-                  }}
-                  disabled={
-                    shopifyUrl.length === 0 || shopifyApiKey.length === 0
-                  }
-                >
-                  Continue
-                </Button>
-              </Box>
-            </Box>
-          )}
-          {activeStep === 2 && (
-            <Box sx={{ width: "440px" }}>
-              <OnboardingTitle sx={{ textAlign: "left" }}>
-                Set your calendar URL.
-              </OnboardingTitle>
-              <OnboardingBody sx={{ mt: 2, textAlign: "left" }}>
-                By clicking on the button below you agree to our privacy policy.
-              </OnboardingBody>
-
-              <TextField
-                sx={{ mt: 6 }}
-                label="Calendar URL"
-                placeholder="https://cal.com/some-calendar"
-                helperText="Please enter your calendar URL"
-                value={calendarUrl}
-                onChange={(e) => setCalendarUrl(e.target.value)}
-              />
-
-              <Button
-                sx={{ mt: 4 }}
-                onClick={() => {
-                  onCreateWorkspace?.({
-                    name: workspaceName,
-                    timezone,
-                    calendarUrl,
-                    shopifyUrl,
-                    shopifyApiKey
-                  });
-                }}
-                disabled={
-                  workspaceName.length === 0 ||
-                  shopifyUrl.length === 0 ||
-                  shopifyApiKey.length === 0 ||
-                  calendarUrl.length === 0
-                }
-              >
-                Create Workspace
-              </Button>
-            </Box>
-          )}
-        </Box>
-      </Box>
-    </Box>
+          </Box>
+        </Form>
+      )}
+    </Formik>
   );
 };
