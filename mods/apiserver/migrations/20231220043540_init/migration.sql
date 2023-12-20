c7f63008ce43085d97a1bd2cc1d65ac3df0ada37
Pedro Sanders <psanders@fonoster.com>
2023-12-19 23:36:09 -0500

    chore: reset db migrations

---
+++ mods/apiserver/migrations/20231220043540_init/migration.sql
@@ -0,0 +1,111 @@
+-- CreateEnum
+CREATE TYPE "workspace_member_status" AS ENUM ('PENDING', 'ACTIVE');
+
+-- CreateEnum
+CREATE TYPE "workspace_member_role" AS ENUM ('OWNER', 'ADMIN', 'MEMBER');
+
+-- CreateEnum
+CREATE TYPE "queue_entry_status" AS ENUM ('ONLINE', 'OFFLINE', 'DEQUEUED', 'IN_PROGRESS');
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "email" VARCHAR(255) NOT NULL,
+    "password_hash" TEXT NOT NULL,
+    "name" VARCHAR(60) NOT NULL,
+    "avatar" VARCHAR(255),
+    "created_at" TIMESTAMPTZ(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "workspaces" (
+    "id" TEXT NOT NULL,
+    "name" VARCHAR(60) NOT NULL,
+    "hours_of_operation" JSONB NOT NULL DEFAULT '{}',
+    "enabled" BOOLEAN NOT NULL DEFAULT true,
+    "calendar_url" VARCHAR(255) NOT NULL,
+    "timezone" TEXT NOT NULL,
+    "created_at" TIMESTAMPTZ(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "ownerId" TEXT NOT NULL,
+
+    CONSTRAINT "workspaces_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "workspace_members" (
+    "id" TEXT NOT NULL,
+    "status" "workspace_member_status" NOT NULL,
+    "role" "workspace_member_role" NOT NULL DEFAULT 'MEMBER',
+    "userId" TEXT NOT NULL,
+    "workspaceId" TEXT NOT NULL,
+    "created_at" TIMESTAMPTZ(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "workspace_members_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "queue_entries" (
+    "id" SERIAL NOT NULL,
+    "customerId" TEXT NOT NULL,
+    "created_at" TIMESTAMPTZ(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "registered_at" TIMESTAMPTZ(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "status" "queue_entry_status" NOT NULL,
+    "metadata" JSONB NOT NULL DEFAULT '{}',
+    "workspaceId" TEXT NOT NULL,
+
+    CONSTRAINT "queue_entries_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "shopify_accounts" (
+    "id" TEXT NOT NULL,
+    "access_token" VARCHAR(255) NOT NULL,
+    "store_domain" VARCHAR(255) NOT NULL,
+    "workspaceId" TEXT NOT NULL,
+    "createdAt" TIMESTAMPTZ(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMPTZ(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "shopify_accounts_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE INDEX "users_email_idx" ON "users" USING HASH ("email");
+
+-- CreateIndex
+CREATE INDEX "workspaces_name_idx" ON "workspaces" USING HASH ("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "workspace_members_userId_workspaceId_key" ON "workspace_members"("userId", "workspaceId");
+
+-- CreateIndex
+CREATE INDEX "queue_entries_workspaceId_idx" ON "queue_entries" USING HASH ("workspaceId");
+
+-- CreateIndex
+CREATE INDEX "queue_entries_customerId_idx" ON "queue_entries" USING HASH ("customerId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "shopify_accounts_workspaceId_key" ON "shopify_accounts"("workspaceId");
+
+-- AddForeignKey
+ALTER TABLE "workspaces" ADD CONSTRAINT "workspaces_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "workspace_members" ADD CONSTRAINT "workspace_members_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "workspace_members" ADD CONSTRAINT "workspace_members_workspaceId_fkey" FOREIGN KEY ("workspaceId") REFERENCES "workspaces"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "queue_entries" ADD CONSTRAINT "queue_entries_workspaceId_fkey" FOREIGN KEY ("workspaceId") REFERENCES "workspaces"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "shopify_accounts" ADD CONSTRAINT "shopify_accounts_workspaceId_fkey" FOREIGN KEY ("workspaceId") REFERENCES "workspaces"("id") ON DELETE CASCADE ON UPDATE CASCADE;
